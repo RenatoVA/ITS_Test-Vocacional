3f4051168a1bd0f5210600e734b28f45aece1d42
Eduardo Puglisevich <eduardopugli2508@gmail.com>
2023-06-25 04:34:34 -0500

    Mejora de info universidades y catalogo

--- static/js/ajax_uInfo.js
+++ static/js/ajax_uInfo.js
@@ -1,4 +1,4 @@
-function htmlCarreras(){
+function htmlUInfo(html){
     /* Solo hay variables o constantes en JS */
     const xhttp = new XMLHttpRequest(); //Un objeto request para un servicio 
     xhttp.onload = function(){
@@ -7,44 +7,19 @@ function htmlCarreras(){
         //alert(this.responseText);
         document.getElementById("result-option").innerHTML = this.responseText
     }
-    xhttp.open("GET","carreras.html",true); //Te conecta por GET a la página de forma asíncrona
+    xhttp.open("GET",html,true); //Te conecta por GET a la página de forma asíncrona
+    xhttp.send();
+}
+function htmlModalidades(html){
+    /* Solo hay variables o constantes en JS */
+    const xhttp = new XMLHttpRequest(); //Un objeto request para un servicio 
+    xhttp.onload = function(){
+        // Aquí se obtiene la respuesta del servicio 
+        //tihs.responseText
+        //alert(this.responseText);
+        document.getElementById("modalidades").innerHTML = this.responseText
+    }
+    xhttp.open("GET",html,true); //Te conecta por GET a la página de forma asíncrona
     xhttp.send();
 }
 
-function htmlAdmision(){
-    /* Solo hay variables o constantes en JS */
-    const xhttp = new XMLHttpRequest(); //Un objeto request para un servicio 
-    xhttp.onload = function(){
-        // Aquí se obtiene la respuesta del servicio 
-        //tihs.responseText
-        //alert(this.responseText);
-        document.getElementById("result-option").innerHTML = this.responseText
-    }
-    xhttp.open("GET","admision.html",true); //Te conecta por GET a la página de forma asíncrona
-    xhttp.send();
-}
-function htmlBeneficios(){
-    /* Solo hay variables o constantes en JS */
-    const xhttp = new XMLHttpRequest(); //Un objeto request para un servicio 
-    xhttp.onload = function(){
-        // Aquí se obtiene la respuesta del servicio 
-        //tihs.responseText
-        //alert(this.responseText);
-        document.getElementById("result-option").innerHTML = this.responseText
-    }
-    xhttp.open("GET","beneficios.html",true); //Te conecta por GET a la página de forma asíncrona
-    xhttp.send();
-}
-
-function htmlContacto(){
-    /* Solo hay variables o constantes en JS */
-    const xhttp = new XMLHttpRequest(); //Un objeto request para un servicio 
-    xhttp.onload = function(){
-        // Aquí se obtiene la respuesta del servicio 
-        //tihs.responseText
-        //alert(this.responseText);
-        document.getElementById("result-option").innerHTML = this.responseText
-    }
-    xhttp.open("GET","contacto.html",true); //Te conecta por GET a la página de forma asíncrona
-    xhttp.send();
-}
